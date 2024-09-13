d3b553b36947da6f0cceedfc624fb8e5f9fffee8
yeswanth <yswnth.2010@gmail.com>
2024-09-13 22:11:33 +0530

    terraform practice

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,36 +1,34 @@
 resource "aws_instance" "terraform" {
 
-    ami = var.ami_id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
-    tags = {
-        Name = "terraform"
-    }
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+  tags                   = var.tags
 }
 
 
 resource "aws_security_group" "allow_ssh_terraform" {
-    name = "allow_ssh"
-    description = "allow ssh 22 ffrom terraform"
+  name        = var.sg_name
+  description = var.sg_description
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = [ "0.0.0.0/0" ]
-        ipv6_cidr_blocks = [ "::/0" ]
-    }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0" ]
-        ipv6_cidr_blocks = [ "::/0" ]
-    }
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = var.protocol
+    cidr_blocks      = var.ingress_cidr
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-    tags = {
-      "Name" = "allow_ssh" 
-    }
-  
+  tags = {
+    "Name" = "allow_ssh"
+  }
+
 }
--- variables/variables.tf
+++ variables/variables.tf
@@ -1,6 +1,51 @@
 variable "ami_id" {
-    type = string
-    default = "ami-09c813fb71547fc4f"  
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+  description = "This is the AMI id of devops-practice based on RHEL-9"
 }
 
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
 
+variable "tags" {
+  type = map(any)
+  default = {
+    Name        = "backend"
+    Project     = "Expense"
+    Component   = "backend"
+    Environment = "DEV"
+    Terraform   = "true"
+  }
+}
+
+variable "sg_name" {
+  type    = string
+  default = "allow_ssh"
+}
+
+variable "sg_description" {
+  type    = string
+  default = "allow ssh 22 ffrom terraform"
+}
+
+variable "from_port" {
+  type    = number
+  default = 22
+}
+
+variable "to_port" {
+  type    = number
+  default = 22
+}
+
+variable "protocol" {
+  type    = string
+  default = "tcp"
+}
+
+variable "ingress_cidr" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
